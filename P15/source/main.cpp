a93834ce768570277e409f42c775f7d0cda038e1
snowdropluna <yichen.jenny0429@gmail.com>
2020-11-12 02:53:28 +0800

    HW3-1

---
+++ P15/source/main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maxmum(int x, int y, int z);
+
+int main(void) 
+{
+	int number1;
+	int number2;
+	int number3;
+	printf("Enter three integers:");
+	scanf_s("%d %d %d", &number1, &number2, &number3);
+	printf("Maxmum is: %d\n", maxmum(number1, number2, number3));
+	system("pause");
+	return 0;
+}
+
+int maxmum(int x, int y, int z) 
+{
+	int max = x;
+	if (y > max) 
+	{
+		max = y;
+	}
+	if (z > max) 
+	{
+		max = z;
+	}
+
+	return max;
+}
